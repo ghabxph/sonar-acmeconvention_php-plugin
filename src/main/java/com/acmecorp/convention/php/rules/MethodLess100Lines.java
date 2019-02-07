e746cf101c7d754f12229cfe4cd493cf21941864
ghabxph <ghabxph.official@gmail.com>
2019-02-08 05:27:54 +0800

    Checks if method declaration is an internal syntax and not a block tree.

--- src/main/java/com/acmecorp/convention/php/rules/MethodLess100Lines.java
+++ src/main/java/com/acmecorp/convention/php/rules/MethodLess100Lines.java
@@ -2,8 +2,8 @@ package com.acmecorp.convention.php.rules;
 
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
-import org.sonar.php.tree.impl.statement.BlockTreeImpl;
 import org.sonar.plugins.php.api.tree.declaration.MethodDeclarationTree;
+import org.sonar.plugins.php.api.tree.lexical.SyntaxToken;
 import org.sonar.plugins.php.api.tree.statement.BlockTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
 
@@ -13,10 +13,10 @@ import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
  * @author ghabxph (ghabxph.official@gmail.com)
  */
 @Rule(
-    priority = Priority.MAJOR,
-    key = "MethodLess100Lines",
-    name = "Method should have less than 100 lines of code",
-    tags = {"convention"}
+        priority = Priority.MAJOR,
+        key = "MethodLess100Lines",
+        name = "Method should have less than 100 lines of code",
+        tags = {"convention"}
 )
 public class MethodLess100Lines extends PHPVisitorCheck {
 
@@ -27,6 +27,11 @@ public class MethodLess100Lines extends PHPVisitorCheck {
      */
     @Override
     public void visitMethodDeclaration(MethodDeclarationTree tree) {
+
+        if (tree.body() instanceof SyntaxToken) {
+            super.visitMethodDeclaration(tree);
+            return;
+        }
 
         BlockTree block = (BlockTree)tree.body();
 
