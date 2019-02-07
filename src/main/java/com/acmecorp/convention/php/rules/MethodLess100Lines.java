73c2bcf35a15c29baa921c29543d513b084e2b10
ghabxph <ghabxph.official@gmail.com>
2019-02-07 15:32:46 +0800

    Added @rule

--- src/main/java/com/acmecorp/convention/php/rules/MethodLess100Lines.java
+++ src/main/java/com/acmecorp/convention/php/rules/MethodLess100Lines.java
@@ -1,5 +1,7 @@
 package com.acmecorp.convention.php.rules;
 
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
 import org.sonar.php.tree.impl.statement.BlockTreeImpl;
 import org.sonar.plugins.php.api.tree.declaration.MethodDeclarationTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
@@ -9,6 +11,12 @@ import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
  *
  * @author ghabxph (ghabxph.official@gmail.com)
  */
+@Rule(
+    priority = Priority.MAJOR,
+    key = "MethodLess100Lines",
+    name = "Method should have less than 100 lines of code",
+    tags = {"convention"}
+)
 public class MethodLess100Lines extends PHPVisitorCheck {
 
     /**
