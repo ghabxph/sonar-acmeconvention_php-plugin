23d1a30d68dd9826166c2d01c8c9ea23a030fcb4
ghabxph <ghabxph.official@gmail.com>
2019-02-10 02:32:58 +0800

    Done Rule: Forbids the use of PHP Code Execution Functions
      - Can now check preg_replace with e modifier.
      - It also checks risky preg_replace (pattern is a variable)
         - This is prone to false positive. Maybe I'll improve this in the future... Maybe not.

--- src/test/resources/checks/ForbidPhpCodeExecution.php
+++ src/test/resources/checks/ForbidPhpCodeExecution.php
@@ -2,5 +2,6 @@
 
 create_function('');                                                      // NOK
 eval('');                                                                 // NOK
-preg_replace("/{(\w*)}/aeda", '$this->TemplateGlobals["$1"]', $content);  // NOK
-preg_replace("/{(\w*)}/", '$this->TemplateGlobals["$1"]', $content);      // OK
+preg_replace($vuln, $payload, $content);                                  // NOK  - Risky
+preg_replace("/{(\w*)}/aeda", '$this->TemplateGlobals["$1"]', $content);  // NOK  - Risky
+preg_replace("/{(\w*)}/", '$this->TemplateGlobals["$1"]', $content);      // OK   - Just fine.
